45bd21963b08123fdf43b2c530c97be3ec650bf8
Santiago Rulfi <santiago.rulfi@wunderman.com>
2018-04-22 20:35:00 -0300

    default thumbnail when not correctly set in json response

--- src/config/index.js
+++ src/config/index.js
@@ -2,4 +2,5 @@ export default {
   devMode: true,
   API_ENDPOINT: 'https://www.reddit.com/top.json?limit=',
   DEFAULT_POSTS_LIMIT: 50,
+  DEFAULT_THUMBNAIL: 'https://mk-lightning.co.uk/wp-content/uploads/2017/09/PlaceholderImg.jpg',
 }
