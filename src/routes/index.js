a19f8775da9ad58809aff08e5ff7698187f488b3
Santiago Rulfi <santiago.rulfi@wunderman.com>
2018-04-22 22:09:36 -0300

    navbar styles

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,12 +9,15 @@ import {
 import Posts from '../containers/Posts'
 import PostDetail from '../containers/PostDetail'
 
+import styles from './styles'
+import Colors from '../themes/colors'
+
 const RouterComponent = () => {
   return (
-    <Router>
+    <Router navigationBarStyle={styles.navBar} titleStyle={styles.navBarTitle}>
       <Stack>
         <Scene key='posts' component={Posts} title='Reddit Posts' />
-        <Scene key='postDetail' component={PostDetail} />
+        <Scene key='postDetail' component={PostDetail} headerTintColor={Colors.white} />
       </Stack>
     </Router>
   )
---
+++ src/routes/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+import { Colors, FontsSizes, Metrics } from '../themes'
+
+export default StyleSheet.create({
+  navBar: {
+    backgroundColor: Colors.darkGray,
+  },
+  navBarTitle: {
+    color: Colors.white,
+  },
+})
